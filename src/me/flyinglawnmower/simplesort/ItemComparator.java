985d2d7d851913ccd7fe91154cab864c95afe4a7
Pyr0Byt3 <stealth1029@yahoo.com>
2012-03-19 20:49:24 -0300

    Split the classes into separate files and added Spout support.

--- src/me/flyinglawnmower/simplesort/ItemComparator.java
+++ src/me/flyinglawnmower/simplesort/ItemComparator.java
@@ -1,1 +1,35 @@
- 
+package me.flyinglawnmower.simplesort;
+
+import java.util.Comparator;
+
+import org.bukkit.inventory.ItemStack;
+
+public class ItemComparator implements Comparator<ItemStack> {
+ public int compare(ItemStack item1, ItemStack item2) {
+		if (item1 == null && item2 != null) {
+			return 1;
+		} else if (item1 != null && item2 == null) {
+			return -1;
+		} else if (item1 == null && item2 == null) {
+			return 0;
+		} else if (item1.getTypeId() > item2.getTypeId()) {
+			return 1;
+		} else if (item1.getTypeId() < item2.getTypeId()) {
+			return -1;
+		} else if (item1.getTypeId() == item2.getTypeId()) {
+			if (item1.getDurability() > item2.getDurability()) {
+				return 1;
+			} else if (item1.getDurability() < item2.getDurability()) {
+				return -1;
+			} else if (item1.getDurability() == item2.getDurability()) {
+				return 0;
+			}
+			if (item1.getAmount() > item2.getAmount()) {
+				return 1;
+			} else if (item1.getAmount() < item2.getAmount()) {
+				return -1;
+			}
+		}
+		return 0;
+	}
+}
