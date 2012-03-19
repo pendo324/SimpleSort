905b05352eb8a1bc76497403887e902db96498d3
Pyr0Byt3 <stealth1029@yahoo.com>
2012-03-19 20:50:03 -0300

    Split the classes into separate files and added Spout support.

--- src/me/flyinglawnmower/simplesort/SpoutStuff.java
+++ src/me/flyinglawnmower/simplesort/SpoutStuff.java
@@ -1,1 +1,10 @@
- 
+package me.flyinglawnmower.simplesort;
+
+import org.getspout.spoutapi.SpoutManager;
+import org.getspout.spoutapi.keyboard.Keyboard;
+
+public class SpoutStuff {
+ public static void registerKeys(SimpleSort plugin) {
+		SpoutManager.getKeyBindingManager().registerBinding("Sort", Keyboard.KEY_K, "Sorts the currently open inventory screen.", new KeyListener(plugin), plugin);
+	}
+}
